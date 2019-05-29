402f13fcf3adf9cbc972efd08cdc506e3c49a81a
bradtaniguchi <bradtaniguchi@gmail.com>
2019-05-28 22:05:25 -0700

    Started to connect header components

--- projects/main-client/src/app/app.component.ts
+++ projects/main-client/src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Group } from './models/group';
 
 @Component({
   selector: 'app-root',
   template: `
-    <app-header></app-header>
+    <app-header
+      [group]="group$ | async"
+      (toggleMenu)="onToggleMenu()"
+    ></app-header>
     <router-outlet></router-outlet>
   `,
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  public group$: Observable<Group | undefined>;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.group$ = of();
+  }
+
+  public onToggleMenu() {
+    console.log('toggle menu');
+  }
+}
--- projects/main-client/src/app/core/header/header.component.ts
+++ projects/main-client/src/app/core/header/header.component.ts
@@ -3,14 +3,16 @@ import {
   OnInit,
   ChangeDetectionStrategy,
   Output,
-  EventEmitter
+  EventEmitter,
+  Input
 } from '@angular/core';
+import { Group } from '../../models/group';
 
 @Component({
   selector: 'app-header',
   template: `
     <mat-toolbar>
-      <mat-toolbar-row fxLayout="row" fxLayoutAlign="space-between">
+      <mat-toolbar-row fxLayout="row" fxLayoutAlign="space-between center">
         <span>
           <button
             type="button"
@@ -25,6 +27,11 @@ import {
           <a routerLink="/" class="mat-title">
             Flare
           </a>
+          <ng-container *ngIf="group">
+            <a class="mat-title" fxHide.xs>
+              {{ group.name }}
+            </a>
+          </ng-container>
         </span>
         <span>
           <!-- USER ICON GOES HERE-->
@@ -37,6 +44,7 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent implements OnInit {
+  @Input() group: Group;
   @Output() toggleMenu = new EventEmitter();
   constructor() {}
 
