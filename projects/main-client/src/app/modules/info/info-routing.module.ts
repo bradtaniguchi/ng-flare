582427114325fcb40c52b2e6e821d3753ae1390f
bradtaniguchi <bradtaniguchi@gmail.com>
2019-06-04 22:09:45 -0700

    Added info component back

--- projects/main-client/src/app/modules/info/info-routing.module.ts
+++ projects/main-client/src/app/modules/info/info-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InfoComponent } from './info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: InfoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
